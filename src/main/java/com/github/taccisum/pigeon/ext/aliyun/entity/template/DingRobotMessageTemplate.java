81bf947d88cb8ceb51b61131d2c839c6e8fa63c5
taccisum <liaojinfeng@deepexi.com>
2022-01-21 14:23:12 +0800

    chore: robust

--- src/main/java/com/github/taccisum/pigeon/ext/aliyun/entity/template/DingRobotMessageTemplate.java
+++ src/main/java/com/github/taccisum/pigeon/ext/aliyun/entity/template/DingRobotMessageTemplate.java
@@ -1,6 +1,8 @@
 package com.github.taccisum.pigeon.ext.aliyun.entity.template;
 
 import com.github.taccisum.pigeon.core.entity.core.MessageTemplate;
+import com.github.taccisum.pigeon.core.valueobj.MessageInfo;
+import org.apache.commons.csv.CSVRecord;
 
 /**
  * 钉钉机器人消息模板
@@ -11,5 +13,15 @@ import com.github.taccisum.pigeon.core.entity.core.MessageTemplate;
 public abstract class DingRobotMessageTemplate extends MessageTemplate {
     public DingRobotMessageTemplate(Long id) {
         super(id);
+    }
+
+    @Override
+    protected String getAccountHeaderName() {
+        throw new UnsupportedOperationException("钉钉消息模板暂不支持此操作");
+    }
+
+    @Override
+    protected MessageInfo map(CSVRecord row, MessageInfo def) {
+        throw new UnsupportedOperationException("钉钉消息模板暂不支持此操作");
     }
 }
